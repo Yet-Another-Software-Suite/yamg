40edbbbf5a4985c0b3bda8e96018ef7e6f08bf0a
DanPeled <98838880+DanPeled@users.noreply.github.com>
2025-08-14 00:47:55 +0300

    removed unused calendar component, seperated code to files (#30)

--- lib/motor-controllers/index.ts
+++ lib/motor-controllers/index.ts
@@ -1,41 +1,42 @@
-import * as SparkMAX from "./spark-max"
-import * as SparkFlex from "./spark-flex"
-import * as TalonFX from "./talon-fx"
-import * as TalonFXS from "./talon-fxs"
-import * as ThriftyNova from "./thrifty-nova"
-import * as ReduxNitrate from "./redux-nitrate"
-import { getMotorController, getWPILibMotorType } from "@/lib/config/hardware-config"
+import * as SparkMAX from "./spark-max";
+import * as SparkFlex from "./spark-flex";
+import * as TalonFX from "./talon-fx";
+import * as TalonFXS from "./talon-fxs";
+import * as ThriftyNova from "./thrifty-nova";
+import * as ReduxNitrate from "./redux-nitrate";
+import {
+  getMotorController,
+  getWPILibMotorType,
+} from "@/lib/config/hardware-config";
 
-export const getMotorControllerModule = (type: string) => {
-  switch (type) {
-    case "SparkMAX":
-      return SparkMAX
-    case "SparkFlex":
-      return SparkFlex
-    case "TalonFX":
-      return TalonFX
-    case "TalonFXS":
-      return TalonFXS
-    case "ThriftyNova":
-      return ThriftyNova
-    case "ReduxNitrate":
-      return ReduxNitrate
-    default:
-      throw new Error(`Unknown motor controller type: ${type}`)
-  }
+const motorModules = {
+  SparkMAX,
+  SparkFlex,
+  TalonFX,
+  TalonFXS,
+  ThriftyNova,
+  ReduxNitrate,
+} as const;
+
+export type MotorType = keyof typeof motorModules;
+
+export function getMotorControllerModule<T extends MotorType>(
+  type: T,
+): (typeof motorModules)[T] {
+  return motorModules[type];
 }
 
 export const getMotorType = (type: string) => {
-  return getWPILibMotorType(type)
-}
+  return getWPILibMotorType(type);
+};
 
 // Check if a motor controller is a REV controller (SparkMAX or SparkFlex)
 export const isRevController = (type: string) => {
-  return type === "SparkMAX" || type === "SparkFlex"
-}
+  return type === "SparkMAX" || type === "SparkFlex";
+};
 
 // Check if a motor controller supports supply current limits
 export const supportsSupplyCurrentLimit = (type: string) => {
-  const controller = getMotorController(type)
-  return controller.supportsSupplyCurrentLimit
-}
+  const controller = getMotorController(type);
+  return controller.supportsSupplyCurrentLimit;
+};
